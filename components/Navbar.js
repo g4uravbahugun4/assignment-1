fc5a533a1fe9957c3b92e64843a689eb1771274f
0iqbeginner <86001972+Daulatraam@users.noreply.github.com>
2022-09-06 17:36:53 +0530

    added README.md

---
+++ components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+   <main className='w-full bg-gray-50 flex   sticky  justify-center p-3 '>
+
+ 
+    
+    <a className='p-4 text-center  font-semibold   hover:cursor-pointer  text-base hover:text-xl '>Service</a>
+    <a className='p-4 text-center  font-semibold   hover:cursor-pointer  text-base hover:text-xl'>Product</a>
+    <a className='p-4 text-center  font-semibold   hover:cursor-pointer  text-base hover:text-xl'>Technology</a>
+    <a className='p-4 text-center font-semibold   hover:cursor-pointer  text-base hover:text-xl'>About</a>
+    <a className='p-4 text-center font-semibold   hover:cursor-pointer  text-base hover:text-xl'>Client</a>
+    <a className='p-4 text-center  font-semibold   hover:cursor-pointer  text-base hover:text-xl'>Patner</a>
+ 
+    <a  className='pt-4  pl-20  font-semibold   hover:cursor-pointer  '> <img src='\icons8-home-25.png'  className='  hover:w-8 hover:h-8  w-6 h-6'/></a>
+    <a  className='pt-4 px-2  font-semibold   hover:cursor-pointer   hover:text-xl'> <img src='\icons8-new-message-24.png'  className=' hover:w-8 hover:h-8 w-6 h-6' /></a>
+
+   </main>
+  )
+}
+ 
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,278 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+
+function Index() {
+ 
+
+  return (
+<>
+ <Navbar />
+    <div className=' flex flex-col'>
+      <div className='flex w-full  justify-center items-center '>
+        <div  className='w-1/2' > 
+<h1  className='px-14 text-4xl font-semibold'>
+Making the most of the ever-growing 
+</h1> <h1  className='px-14 text-4xl text-red-500 font-semibold'>Information Technology</h1>
+
+<p className='px-14 pt-5'>
+Managed by a team of professional experts with extensive experience and impressive track records
+</p>
+<span className='p-8 w-4/5 flex justify-center items-center '>
+              <button className='px-4 py-2 rounded-md shadow-md hover:bg-rose-700   text-white border bg-rose-500 '>
+  Reade More
+</button>
+</span>
+
+        </div> 
+        <div className='w-1/2  '>
+         <img src='/pana.png' className='w-4/5'  />
+        </div>
+      
+
+      </div>
+   <div  className='flex  '>
+
+ 
+   <div className='flex w-full  bg-slate-100   '>
+        
+   <div className='w-1/2 flex justify-end '>
+         <img src='/pana-1.png' className='w-4/5 h-4/5  '  />
+        </div>
+        <div  className='w-1/2 flex flex-col  justify-center items-center' > 
+<h1  className='px-14 text-3xl font-semibold'>
+Welcome to <font color="#FF2626">Dwidasa Samsara Indonesia (DSI)</font>
+      
+</h1>
+<p className='px-14 pt-5 '>
+Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 
+by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through <font color="#FF2626"> (DSI)</font>’s distinct front-end based application concept. </p><p className='px-14 pt-5 '>  Managed by a team of professional experts with extensive experience and impressive track records,<font color="#FF2626"> (DSI)</font>  believes that continuous improvements and innovations assure your business to run effectively and efficiently.
+</p>
+<span className='p-8 w-4/5 flex justify-center items-center '>
+                <button className='px-4 py-2 rounded-md shadow-md hover:bg-rose-700   text-white border bg-rose-500 '>
+  Reade More
+</button>
+</span>
+
+        </div> 
+       
+      
+
+      </div>
+
+
+   </div>
+   <div className='flex w-full justify-center items-center'>
+   <h1 className='text-3xl pt-44 pb-16'>
+    Product and Service
+    </h1>
+    </div>
+   <div   className='flex md:flex md:flex-col  justify-between  '>
+
+   
+   <div className='flex flex-col    justify-center items-center '>
+        
+        <div className='w-auto '>
+              <img src='\product-description 1.png' className='w-20'  />
+             </div>
+             <div  className='' > 
+     <h1  className='px-14 text-center pt-2 text-2xl font-semibold text-red-600'>
+     Our Product
+     </h1>
+     <p className='px-14 pt-5'>
+     Our product is made on the base of our team’s careful research and analyses, ranging from internet based application.
+     </p>
+     <div className='p-8  flex justify-center items-center '>
+     <button className='px-4 py-2 rounded-md shadow-md hover:bg-rose-700  text-white border bg-rose-500 '>
+  Reade More
+</button>
+     </div>
+     
+             </div> 
+     </div>
+
+     <div className='flex flex-col   justify-center items-center '>
+        
+            <div className='w-auto '>
+              <img src='\product-description 2.png' className='w-20'/>
+             </div>
+             <div  className='' > 
+              <h1 className='px-14 pt-2 text-center text-2xl font-semibold text-red-600'>
+     Our Service
+     </h1>
+     <p className='px-14 pt-5'>
+     DSI’s shared service solutions focus on the front-end based software development, designed specifically for the banking and financial sectors. 
+     </p>
+     <div className='p-8  flex justify-center items-center '>
+                <button className='px-4 py-2 rounded-md shadow-md hover:bg-rose-700   text-white border bg-rose-500 '>
+  Reade More
+</button>
+     </div>
+     
+             </div> 
+  </div>
+
+
+  <div className='flex flex-col  justify-center items-center '>
+        
+            <div className='w-auto '>
+              <img src='\product-description 3.png' className='w-20'  />
+             </div>
+             <div  className='' > 
+              <h1 className='px-14 pt-2 text-center text-2xl font-semibold text-red-600'>
+     Our Technology
+     </h1>
+     <p className='px-14 pt-5'>
+     First JAVA, runs on more than 850 million personal computers worldwide, and on billions of devices worldwide, including mobile and TV devices.
+     </p>
+     <div className='p-8  flex justify-center items-center '>
+                <button className='px-4 py-2 rounded-md shadow-md hover:bg-rose-700  text-white border bg-rose-500 '>
+  Reade More
+</button>
+     </div>
+     
+             </div> 
+
+           </div>
+
+
+
+     
+   </div>
+
+   <div className='flex w-full justify-center items-center'>
+   <h1 className='text-2xl p-6'>
+   Mobile Specialist
+    </h1>
+    </div>
+   <div   className='flex justify-center md:flex md:flex-col md:justify-evenly  items-center  w-full  '>
+
+<div className=' p-2 '>
+<div className='flex flex-col  border shadow-md rounded-lg   justify-center items-center '>
+     
+     <div className='w-44  pt-5   '>
+           <img src='/pana-1.png' className=' w-full border shadow-md rounded-full'  />
+          </div>
+          <div  className='flex flex-col justify-center items-center' > 
+  <h1  className=' pt-2 text-base font-semibold text-red-600'>
+  SystemAnalyst
+  </h1>
+  <p className=' text-xl pt-2'>
+  RodneyStratton
+  </p>
+  <span className='p-8 w-4/5 flex justify-center items-center '>
+                  <a className='px-2'> <img src='\icons8-facebook-24.png ' className='hover:cursor-pointer hover:w-10  w-8 h-5'/></a>
+                  <a className='px-2'> <img src='\icons8-instagram-24.png' className='hover:w-10 hover:cursor-pointer w-8 h-5' />
+                  </a> <a className='px-2'><img src='\icons8-twitter-30.png' className='hover:w-10 hover:cursor-pointer w-8 h-5' />
+  </a></span>
+  
+          </div> 
+  </div>
+   </div>
+   <div className=' p-2 '>
+<div className='flex flex-col  border shadow-md rounded-lg   justify-center items-center '>
+     
+     <div className='w-44  pt-5   '>
+           <img src='/pana-1.png' className='w-full border shadow-md rounded-full'  />
+          </div>
+          <div  className='flex flex-col justify-center items-center' > 
+  <h1  className=' pt-2 text-base font-semibold text-red-600'>
+  SystemAnalyst
+  </h1>
+  <p className=' text-xl pt-2'>
+  RodneyStratton
+  </p>
+  <span className='p-8 w-4/5 flex justify-center items-center '>
+                  <a className='px-2'> <img src='\icons8-facebook-24.png' className='hover:w-10 hover:cursor-pointer w-8 h-5'/></a>
+                  <a className='px-2'> <img src='\icons8-instagram-24.png' className='hover:w-10 hover:cursor-pointer w-8 h-5' />
+                  </a> <a className='px-2'><img src='\icons8-twitter-30.png' className='hover:w-10 hover:cursor-pointer w-8 h-5' />
+  </a>
+  </span>
+  
+          </div> 
+  </div>
+   </div>
+   <div className=' p-2 '>
+<div className='flex flex-col  border shadow-md rounded-lg   justify-center items-center '>
+     
+     <div className='w-44  pt-5   '>
+           <img src='/pana-1.png' className='w-full border shadow-md rounded-full'  />
+          </div>
+          <div  className='flex flex-col justify-center items-center' > 
+  <h1  className=' pt-2 text-base font-semibold text-red-600'>
+  Programmer
+  </h1>
+  <p className=' text-xl pt-2'>
+  RodneyStratton
+  </p>
+  <span className='p-8 w-4/5 flex justify-center items-center '>
+                  <a className='px-2'> <img src='\icons8-facebook-24.png' className='hover:w-10 hover:cursor-pointer w-8 h-5'/></a>
+                  <a className='px-2'> <img src='\icons8-instagram-24.png' className='hover:w-10 hover:cursor-pointer w-8 h-5' />
+                  </a> <a className='px-2'><img src='\icons8-twitter-30.png' className='hover:w-10 hover:cursor-pointer w-8 h-5' />
+  </a>
+  </span>
+  
+          </div> 
+  </div>
+   </div>
+   <div className=' p-2 '>
+<div className='flex flex-col  border shadow-md rounded-lg   justify-center items-center '>
+     
+     <div className='w-44  pt-5   '>
+           <img src='/pana-1.png' className='w-full border shadow-md rounded-full'  />
+          </div>
+          <div  className='flex flex-col justify-center items-center' > 
+  <h1  className=' pt-2 text-base font-semibold text-red-600'>
+  Programmer
+  </h1>
+  <p className=' text-xl pt-2'>
+  RodneyStratton
+  </p>
+  <span className='p-8 w-4/5 flex justify-center items-center '>
+                  <a className='px-2'> <img src='\icons8-facebook-24.png' className='hover:w-10 hover:cursor-pointer w-8 h-5'/></a>
+                  <a className='px-2'> <img src='\icons8-instagram-24.png' className='hover:w-10 hover:cursor-pointer w-8 h-5' />
+                  </a> <a className='px-2'><img src='\icons8-twitter-30.png' className='hover:w-10 hover:cursor-pointer w-8 h-5' />
+  </a>
+  </span>
+  
+          </div> 
+  </div>
+   </div>
+
+  
+</div>
+   <div className='flex mt-10 mb-10 justify-evenly md:flex md:flex-col md:justify-center md:self-center '>
+    <div className='p-3 '>
+  <h1 className='font-semibold text-center'>PT Dwidasa Samsara IndonesiaRuko</h1>   Jalur Sutera 29A No. 53  Alam Sutera Serpong, Tangerang 15323 
+    </div>
+    <div className='p-3 '>
+   <h1 className='font-semibold text-center'>Contact</h1> 
+            <p className='text-center'>Phone : +62.21.5314.1135 </p>
+            <p className='text-center'> Fax : +62.21.5314.1135</p>
+            <p className='text-center'> Email : community@dwidasa.com</p>
+    </div>
+    <div className=' p-3  flex flex-col justify-between self-center '>
+      <span className='w-1/2 flex   '>
+      <img src='\image 7.png' className='h-8 ' />
+      <img src='\image 9.png' className='h-8 ml-8'/>
+      <img src='\image 10.png' className='h-8 ml-10' /></span>
+      <span className='w-1/4 flex '>  
+      <img src='\image 11.png' className='h-10' />
+      <img src='\image 13.png' className='ml-8 mt-4 h-4' />
+      <img src='\image 14.png' className='ml-10 mt-4 h-4' />
+      </span>
+     
+    </div>
+   </div>
+
+<div className='bg-rose-500 text-white flex justify-center items-center h-10'>
+          Copyright &#169;2015
+</div>
+
+    </div></>
+  )
+}
+
+export default Index
